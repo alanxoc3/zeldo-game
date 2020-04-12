f4f7cdbbe65d91009d5b491f5e0f3afbc089191d
Alan Morgan <alanxoc3@gmail.com>
2020-04-12 01:08:46 -0600

    Basic panning and buttons implemented.
    Next I will make it so you can select tiles/spaces :).
    
    After that I will have to add the objects.

--- src/mapbuilder_demo.lua
+++ src/mapbuilder_demo.lua
@@ -1,6 +1,18 @@
 dir_arr = {'l', 'r', 'u', 'd'}
 met_arr = {'template', 'm', 'c', 'w', 'h'}
 
+function sort(t)
+   if t then
+      for n=2,#t do
+         local i=n
+         while i>1 and t[i]<t[i-1] do
+            t[i],t[i-1]=t[i-1],t[i]
+            i=i-1
+         end
+      end
+   end
+end
+
 function sort_by_k(t)
    if t then
       for n=2,#t do
@@ -125,20 +137,108 @@ function array_tostring(any)
 end
 
 -- Here is the logic for the map builder tool.
-function _init()
-   reload(0x0000, 0x0000, 0x4300, "zeldo.p8")
-   music(14)
+function create_button(x, y, text, cb)
+   local hw = #text*2
+   return {
+      x=x, y=y, rx=hw, ry=3, text=text, callback=cb, active=true
+   }
 end
 
-function _update60()
-   if btnp(4) or btnp(5) then
-      printh("g_rooms = gun_vals[".."[\n"..rooms_to_str(g_rooms).."]".."]\n")
-      extcmd("shutdown")
+function did_click_button(b)
+   return b.active and
+      mouse_x < b.x+b.rx and
+      mouse_x > b.x-b.rx and
+      mouse_y < b.y+b.ry and
+      mouse_y > b.y-b.ry
+end
+
+function draw_button(b)
+   if b.active then
+      rectfill(b.x-b.rx,b.y-b.ry,b.x+b.rx,b.y+b.ry,9)
+      print(b.text, b.x-b.rx+1, b.y-b.ry+1, 7)
    end
 end
 
+g_map = {
+   x=0,y=0,w=2,h=2
+}
+
+off_x = 0
+off_y = 0
+
+function _init()
+   reload(0x0000, 0x0000, 0x4300, "zeldo.p8")
+   poke(0x5f2d, 1)
+   music(14)
+
+   mouse_x, mouse_y = 0, 0
+   prev_mouse_x, prev_mouse_y = 0, 0
+
+   g_room_inds = {}
+   g_cur_room_ind = 1
+   for k,v in pairs(g_rooms) do
+      add(g_room_inds, k)
+   end
+   sort(g_room_inds)
+
+   butts = {
+      create_button(115,3, "save", function()
+         printh("g_rooms = gun_vals[".."[\n"..rooms_to_str(g_rooms).."]".."]\n")
+         extcmd("shutdown")
+      end),
+
+      create_button(90,3, "delete", function()
+
+      end)
+   }
+end
+
+function _update60()
+   if btnp(0) then
+      g_cur_room_ind = max(g_cur_room_ind-1, 1)
+   elseif btnp(1) then
+      g_cur_room_ind = min(g_cur_room_ind+1, #g_room_inds)
+   end
+
+   local k = g_room_inds[g_cur_room_ind]
+   local qx, qy = flr(k/10 % 4), flr(k/40)
+   local t_ind = k % 10
+
+   map_x = qx*32 + g_room_template[t_ind].x
+   map_y = qy*32 + g_room_template[t_ind].y
+   map_w = g_rooms[k].w or g_room_template[t_ind].w
+   map_h = g_rooms[k].h or g_room_template[t_ind].h
+   map_c = g_rooms[k].c or 0
+
+   mouse_x = mid(0, stat(32), 128)
+   mouse_y = mid(0, stat(33), 128)
+
+   if not btn(4) then
+      prev_mouse_x, prev_mouse_y = mouse_x, mouse_y
+   else
+      off_x = prev_mouse_x - mouse_x
+      off_y = prev_mouse_y - mouse_y
+   end
+
+   foreach(butts, function(b)
+      if stat(34) == 1 then
+         if did_click_button(b) then
+            b.callback()
+         end
+      end
+   end)
+end
+
 function _draw()
-   cls(8)
-   print('B_UP', 0, 0, 7)
-   spr(0,10,10,10,10,10,10,1,1)
+   cls(map_c)
+
+   map(map_x, map_y, 64-map_w*4-off_x, 64-map_h*4-off_y, map_w, map_h)
+
+   foreach(butts, function(b)
+      draw_button(b)
+   end)
+
+   print("room #"..g_room_inds[g_cur_room_ind], 5, 121, 7)
+
+   spr(0, mouse_x-4, mouse_y-4)
 end
