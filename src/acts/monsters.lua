6dd5281d373715a1f1603fb6aa789b640a97b975
Alan Morgan <alanxoc3@gmail.com>
2021-01-03 20:56:07 -0700

    [GH-6] Removed topy.
    Topy isn't needed until later in the game. Maybe I will improve him too
    at that point.

--- src/acts/monsters.lua
+++ src/acts/monsters.lua
@@ -30,58 +30,58 @@ end, function(a, other, ...)
    call_not_nil(other, 'knockback', other, .2, ...)
 end, look_at_pl)
 
-create_actor([[topy;2;drawable,bounded,danceable,confined,stunnable,mov,col,tcol,hurtable,knockable,anim,spr]], [[
-   max_health:10; health:10;
-   name:"topy";
-   evil:true;
-   x:@1;y:@2;
-   rx:.375;ry:.375;
-   tl_loop:true;
-   iyy:-2;
-   sinds:112,113;
-   anim_len:1;
-   touchable:true;
-   destroyed:@8;
-   hurt_func:@9;
+-- create_actor([[topy;2;drawable,bounded,danceable,confined,stunnable,mov,col,tcol,hurtable,knockable,anim,spr]], [[
+--    max_health:10; health:10;
+--    name:"topy";
+--    evil:true;
+--    x:@1;y:@2;
+--    rx:.375;ry:.375;
+--    tl_loop:true;
+--    iyy:-2;
+--    sinds:112,113;
+--    anim_len:1;
+--    touchable:true;
+--    destroyed:@8;
+--    hurt_func:@9;
+-- 
+--    i=@4,hit=nf,u=nf,tl_max_time=.5;
+--    i=@7,hit=nf,u=@5,tl_max_time=.5;
+--    i=@6,hit=@3,u=nf,tl_max_time=1;
+-- ]], function(a, other, ...)
+--    call_not_nil(other, 'knockback', other, .3, ...)
+-- 
+--    if other.pl then
+--       other.hurt(other, 2, 30)
+--    end
+-- end, function(a) -- init 1 @4
+--    a.ax, a.ay = 0, 0
+--    a.anim_off = 0
+-- end, look_at_pl, -- update 1 @5
+-- function(a) -- init 2 @6
+--    amov_to_actor(a, g_pl, .06)
+-- end, function(a)
+--    a.anim_off = 1
+-- end, function(a)
+--    destroy_effect(a, 30, 1, 4, 5, 2)
+--    _g.money(a.x, a.y, a.dx, a.dy)
+-- end, function(a)
+--    a.tl_next = 2
+--    change_cur_ma(a)
+-- end)
 
-   i=@4,hit=nf,u=nf,tl_max_time=.5;
-   i=@7,hit=nf,u=@5,tl_max_time=.5;
-   i=@6,hit=@3,u=nf,tl_max_time=1;
-]], function(a, other, ...)
-   call_not_nil(other, 'knockback', other, .3, ...)
-
-   if other.pl then
-      other.hurt(other, 2, 30)
-   end
-end, function(a) -- init 1 @4
-   a.ax, a.ay = 0, 0
-   a.anim_off = 0
-end, look_at_pl, -- update 1 @5
-function(a) -- init 2 @6
-   amov_to_actor(a, g_pl, .06)
-end, function(a)
-   a.anim_off = 1
-end, function(a)
-   destroy_effect(a, 30, 1, 4, 5, 2)
-   _g.money(a.x, a.y, a.dx, a.dy)
-end, function(a)
-   a.tl_next = 2
-   change_cur_ma(a)
-end)
-
-create_actor([[kluck;2;drawable,loopable,bounded,confined,stunnable,mov,col,tcol,hurtable,knockable,spr,danceable]], [[
-   name:"kluck";
-   evil:true;
-   x:@1;y:@2;
-   rx:.375;ry:.375;
-   sind:32;
-   destroyed:@4;
-
-   i=@3,tl_max_time=.5;
-]], function(a)
-   a.ax, a.ay = rnd_one(.01), rnd_one(.01)
-   a.xf = a.ax < 0
-end, destroy_func)
+-- create_actor([[kluck;2;drawable,loopable,bounded,confined,stunnable,mov,col,tcol,hurtable,knockable,spr,danceable]], [[
+--    name:"kluck";
+--    evil:true;
+--    x:@1;y:@2;
+--    rx:.375;ry:.375;
+--    sind:32;
+--    destroyed:@4;
+-- 
+--    i=@3,tl_max_time=.5;
+-- ]], function(a)
+--    a.ax, a.ay = rnd_one(.01), rnd_one(.01)
+--    a.xf = a.ax < 0
+-- end, destroy_func)
 
 -- create_actor([['bat', 2, {'bounded','confined','stunnable','mov','col','hurtable','knockable','anim','spr'}]], [[
 --    evil=true,
