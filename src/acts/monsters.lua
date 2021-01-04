c92f94f1d9ce8b7d1c7a064cfd03cd821e9201f2
Alan Morgan <alanxoc3@gmail.com>
2021-01-03 20:35:34 -0700

    [GH-6] Random improvements.
    - Refactored duplicate code from boomerang.
    - Hitting things with a boomerang shows it up in the ma box again (but a bit buggy).
    - Slimy jumps a shorter distance.

--- src/acts/monsters.lua
+++ src/acts/monsters.lua
@@ -10,24 +10,24 @@ create_actor([[slimy;2;drawable,bounded,danceable,confined,stunnable,mov,col,tco
    touchable:true;
    hurt_func:@9;
 
-   i=@5,hit=nf,u=@8,tl_max_time=3;
-   i=nf,hit=nf,u=@3,e=@6,tl_max_time=.25;
-   i=nf,hit=@7,u=@4,e=@6,tl_max_time=.25;
+   i=@5,hit=nf,u=@8,tl_max_time=3;        -- wait
+   i=nf,hit=nf,u=@3,e=@6,tl_max_time=.25; -- shake
+   i=nf,hit=@7,u=@4,e=@6,tl_max_time=.25; -- in air
 ]], function(a)
    look_at_pl(a)
    a.ixx = rnd_one()
 end, function(a) -- init 1 @4
-   amov_to_actor(a, g_pl, .05)
+   amov_to_actor(a, g_pl, .03)
    a.iyy += sin(a.tl_tim/a.tl_max_time)
    a.sind=119
 end, function(a)
    a[1].tl_max_time = rnd(2)+1
    a.ax, a.ay = 0, 0
 end, function(a)
-   a.ixx = 0 a.iyy = -2
+   a.ixx=0 a.iyy=-2
    a.sind=118
 end, function(a, other, ...)
-   call_not_nil(other, 'knockback', other, .4, ...)
+   call_not_nil(other, 'knockback', other, .2, ...)
 end, look_at_pl)
 
 create_actor([[topy;2;drawable,bounded,danceable,confined,stunnable,mov,col,tcol,hurtable,knockable,anim,spr]], [[
@@ -68,6 +68,20 @@ end, function(a)
    a.tl_next = 2
    change_cur_ma(a)
 end)
+
+create_actor([[kluck;2;drawable,loopable,bounded,confined,stunnable,mov,col,tcol,hurtable,knockable,spr,danceable]], [[
+   name:"kluck";
+   evil:true;
+   x:@1;y:@2;
+   rx:.375;ry:.375;
+   sind:32;
+   destroyed:@4;
+
+   i=@3,tl_max_time=.5;
+]], function(a)
+   a.ax, a.ay = rnd_one(.01), rnd_one(.01)
+   a.xf = a.ax < 0
+end, destroy_func)
 
 -- create_actor([['bat', 2, {'bounded','confined','stunnable','mov','col','hurtable','knockable','anim','spr'}]], [[
 --    evil=true,
@@ -114,16 +128,3 @@ end)
 --    end, destroy_func
 -- )
 
-create_actor([[kluck;2;drawable,loopable,bounded,confined,stunnable,mov,col,tcol,hurtable,knockable,spr,danceable]], [[
-   name:"kluck";
-   evil:true;
-   x:@1;y:@2;
-   rx:.375;ry:.375;
-   sind:32;
-   destroyed:@4;
-
-   i=@3,tl_max_time=.5;
-]], function(a)
-   a.ax, a.ay = rnd_one(.01), rnd_one(.01)
-   a.xf = a.ax < 0
-end, destroy_func)
