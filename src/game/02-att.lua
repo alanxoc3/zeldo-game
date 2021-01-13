024f681a916f6b8042e7e286c0651e4ce2572f9f
Alan Morgan <alanxoc3@gmail.com>
2021-01-12 22:09:22 -0700

    [GH-36] 7468 Did some serious token trimming on create actor.
    I did some things with the add function and unpack function that I
    didn't know existed until now.

--- src/game/02-att.lua
+++ src/game/02-att.lua
@@ -3,74 +3,54 @@
 
 g_act_arrs = {}
 
-function create_parent_actor_shared(meta_and_att_str, func, ...)
-   local meta, template_str = unpack(split(meta_and_att_str, '|'))
+function create_parent_actor_shared(is_create_parent, meta_and_att_str, ...)
+   local meta, template = unpack(split(meta_and_att_str, '|'))
    local template_params, id, provided, parents, pause_funcs = {...}, unpack(ztable(meta))
 
-   _g[id] = func(
-      function(a, ...)
-         local func_params, params = {...}, {}
-         for i=1, provided do
-            add(params, func_params[i] or false)
-         end
+   _g[id] = function(...)
+      local func_params, params, a = {...}, tabcpy(template_params), {}
+      if is_create_parent then
+         a = deli(func_params, 1)
+      end
 
-         foreach(template_params, function(x)
-            add(params, x)
+      for i=1, provided do
+         add(params, func_params[i] or false, i)
+      end
+
+      -- If the actor/parent isn't set, attach it!
+      if not a[id] then
+         -- step 1: set/call attributes from parents
+         foreach(parents, function(par)
+            if type(par) ~= 'table' then
+               par = {par}
+            end
+            a = _g[par[1]](a, unpack(par,2))
          end)
 
-         a = a or {} -- difference
-         return a[id] and a or attach_actor(id, parents, pause_funcs or {}, template_str, a, params)
+         tabcpy(ztable(template, unpack(params)), a)
+
+         -- step 2: add to list of objects
+         if not a[id] then
+            g_act_arrs[id] = g_act_arrs[id] or {}
+            add(g_act_arrs[id], a)
+         end
+
+         -- step 3: attach timeline
+         a.id, a[id], a.pause = id, true, a.pause or {}
+
+         -- step 4: build up pause functions
+         foreach(pause_funcs, function(f)
+            a.pause[f] = true
+         end)
       end
-   )
+
+      return a
+   end
 end
 
 -- params: str, opts
-function create_parent(meta_and_template_str, ...)
-   create_parent_actor_shared(meta_and_template_str, function(f)
-      return function(a, ...)
-         return f(a, ...)
-      end
-   end, ...)
-end
-
-function create_actor(meta_and_template_str, ...)
-   create_parent_actor_shared(meta_and_template_str, function(f)
-      return function(...)
-         return f({}, ...)
-      end
-   end, ...)
-end
-
--- opt: {id, att, par}
-function attach_actor(id, parents, pause_funcs, template, a, params)
-   -- step 1: atts from parent
-   foreach(parents, function(par)
-      if type(par) ~= 'table' then
-         par = {par}
-      end
-      a = _g[par[1]](a, unpack(par,2))
-   end)
-
-   tabcpy(ztable(template, unpack(params)), a)
-   if a.name == "slimy" then printh("len is: "..#a) end
-
-   -- step 2: add to list of objects
-   if not a[id] then
-      g_act_arrs[id] = g_act_arrs[id] or {}
-      add(g_act_arrs[id], a)
-   end
-
-   -- step 3: build up pause functions
-   a.pause = a.pause or {}
-   foreach(pause_funcs, function(f)
-      a.pause[f] = true
-   end)
-
-   -- step 4: attach timeline
-   a.id, a[id] = id, true
-
-   return a
-end
+function create_parent(...) create_parent_actor_shared(true, ...) end
+function create_actor(...)  create_parent_actor_shared(false, ...) end
 
 function acts_loop(id, func_name, ...)
    for a in all(g_act_arrs[id]) do
